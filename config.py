0b84c0b202a0668ef0649b483d01c3044c13a987
Ariya Rasekh <rasekh@uwindsor.ca>
2022-12-15 15:15:58 -0500

    bug fixed

---
+++ config.py
@@ -0,0 +1,15 @@
+"""
+IMPORTANT: If you are on a linux machine, adjust path formats. / should be used instead of \\
+"""
+
+WORD_DATA_BASE = "https://www.mit.edu/~ecprice/wordlist.10000"
+
+HANDWRITTEN_WORDS_PATH = "data\\images\\hand_written_words\\"
+RAW_HANDWRITTEN_WORDS_PATH = "data\\images\\raw_hand_written_words\\"
+X_RAY_SCAN_PATH = "data\\images\\raw_medical_image\\"
+DATA_OUTPUT = "data\\images\\synthetic_data\\"
+WHOLE_DATA_OUTPUT = "data\\images\\synthetic_data\\"
+DATASET_METADATA_PATH = "data\\"
+
+HANDWRITTEN_WORDS_IDS_PATH = f"{HANDWRITTEN_WORDS_PATH}hand_written_words_names.pkl"
+X_RAY_SCAN_IDS_PATH = f"{X_RAY_SCAN_PATH}scan_ids.pkl"
